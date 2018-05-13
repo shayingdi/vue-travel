f8d2fb0e7930c09120fd6d2ffd9c05221ac35999
shayingdi <shayingdi0213@sina.com>
2018-05-13 19:25:26 +0800

    swiper

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
@@ -8,10 +9,12 @@
   // 多页应用，首屏时间快，seo效果好，但页面切换慢
   // 单页应用，页面切换快，首屏时间稍慢，seo差
   import HomeHeader from './components/Header.vue'
+  import HomeSwiper from './components/Swiper.vue'
   export default {
     name: 'Home',
     components: {
-      HomeHeader
+      HomeHeader,
+      HomeSwiper
     }
   }
 </script>
--- travel/src/pages/home/components/Swiper.vue
+++ travel/src/pages/home/components/Swiper.vue
@@ -1,13 +1,52 @@
 <template>
-  
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src='item.img' alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
 </template>
+
 <script>
-export default {
-  
-}
+  export default {
+    name: 'HomeSwiper',
+    data() {
+      return {
+        swiperOption: {
+          pagination: '.swiper-pagination',
+          loop: true
+        },
+        swiperList: [{
+            id: '001',
+            img: 'http://img1.qunarzz.com/piao/fusion/1804/15/9250dbc86a456302.jpg_750x200_b423f532.jpg'
+          },
+          {
+            id: '002',
+            img: 'http://img1.qunarzz.com/piao/fusion/1805/9a/c0984ec80d0f0a02.jpg_750x200_f55672ab.jpg'
+          },
+          {
+            id: '003',
+            img: 'http://img1.qunarzz.com/piao/fusion/1805/9e/f625886dec6c0402.jpg_750x200_417be630.jpg'
+          }
+        ]
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" scoped>
-
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color #fff
+  .wrapper
+    width 100%
+    height 0
+    overflow hidden
+    padding-bottom 31.25%
+    .swiper-img
+      width 100%
 </style>
 
