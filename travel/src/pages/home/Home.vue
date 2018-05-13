0eaf73ed5bbe382da05fcd542bdaa3d693418ee6
shayingdi <shayingdi0213@sina.com>
2018-05-13 18:33:36 +0800

    save home-header

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    home
-    
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
   // 多页应用，首屏时间快，seo效果好，但页面切换慢
   // 单页应用，页面切换快，首屏时间稍慢，seo差
+  import HomeHeader from './components/Header.vue'
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      HomeHeader
+    }
   }
 </script>
 
 <style>
-  
+
 </style>
