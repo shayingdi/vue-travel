55400ea32cb07f61e378b20ea7649c5f7edce25b
shayingdi <shayingdi0213@sina.com>
2018-05-13 20:49:28 +0800

    add icons

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
@@ -10,11 +11,13 @@
   // 单页应用，页面切换快，首屏时间稍慢，seo差
   import HomeHeader from './components/Header.vue'
   import HomeSwiper from './components/Swiper.vue'
+  import HomeIcons from './components/Icons.vue' 
   export default {
     name: 'Home',
     components: {
       HomeHeader,
-      HomeSwiper
+      HomeSwiper,
+      HomeIcons
     }
   }
 </script>
---
+++ travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page) in pages" :key="page.index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img :src="item.imgUrl" alt="">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeIcons',
+    data() {
+      return {
+        iconList: [{
+            id: '0001',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0002',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0003',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0004',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0005',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0006',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0007',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0008',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0009',
+            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+            desc: '景点门票'
+          },
+        ]
+      }
+    },
+    computed: {
+      pages() {
+        const pages = []
+        this.iconList.forEach((item, index) => {
+          const page = Math.floor(index / 8)
+          if (!pages[page]) {
+            pages[page] = []
+          }
+          pages[page].push(item)
+        })
+        return pages
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  @import '~styles/mixins.styl'
+  .icons >>> .swiper-container
+    width 100%
+    height 0
+    padding-bottom 50%
+  .icon 
+      width 25%
+      height 0
+      float left
+      padding-bottom 25%
+      overflow hidden
+      position relative
+      .icon-img
+        position absolute 
+        left 0
+        top 0
+        bottom .44rem
+        right 0
+        box-sizing border-box
+        padding .1rem
+        img
+          height 100%
+          display block 
+          margin 0 auto 
+      .icon-desc
+        position absolute
+        bottom 0
+        height .44rem
+        line-height .44rem
+        color $darkTextColor
+        text-align center
+        width 100%
+        ellipsis()
+</style>
+
+
