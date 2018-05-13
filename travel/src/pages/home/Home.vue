fe806d09319258e78400f74dbec94be162c775f3
shayingdi <shayingdi0213@sina.com>
2018-05-13 16:30:01 +0800

    save

---
+++ travel/src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    home
+    
+  </div>
+</template>
+
+<script>
+  // 多页应用，首屏时间快，seo效果好，但页面切换慢
+  // 单页应用，页面切换快，首屏时间稍慢，seo差
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style>
+  
+</style>
